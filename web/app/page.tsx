9bc07ffb94b6758ce461f65e582462a0a562a137
Toygun Tez <109132154+Zireaelst@users.noreply.github.com>
2026-07-25 06:48:12 +0100

    feat(web): assemble the full landing page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,11 @@
 import CinematicHero from "@/components/CinematicHero";
 import SiteHeader from "@/components/SiteHeader";
+import ThesisSection from "@/components/ThesisSection";
+import HowItWorksSection from "@/components/HowItWorksSection";
+import ArchitectureSection from "@/components/ArchitectureSection";
+import TracksSection from "@/components/TracksSection";
+import FraudDemoTeaser from "@/components/FraudDemoTeaser";
+import CtaFooter from "@/components/CtaFooter";
 import { ThemeProvider } from "@/lib/theme";
 
 export default function Home() {
@@ -7,6 +13,12 @@ export default function Home() {
     <ThemeProvider>
       <SiteHeader />
       <CinematicHero />
+      <ThesisSection />
+      <HowItWorksSection />
+      <ArchitectureSection />
+      <TracksSection />
+      <FraudDemoTeaser />
+      <CtaFooter />
     </ThemeProvider>
   );
 }
